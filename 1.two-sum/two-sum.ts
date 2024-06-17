9b85ee48ed33a05e531a6094d37c680494cad4fa
Hardy <hardy22110@gmail.com>
2024-06-17 14:32:27 +0800

    Submission: 1.two-sum

--- 1.two-sum/two-sum.ts
+++ 1.two-sum/two-sum.ts
@@ -1,13 +1,11 @@
 export function twoSum(nums: number[], target: number): number[] {
+  const sumMap = new Map()
   for (let i = 0; i < nums.length; i++) {
-    for (let a = 0; a < nums.length; a++) {
-      if (i === a) {
-        continue
-      }
-      if (nums[i] + nums[a] === target) {
-        return [i, a]
-      }
+    let currentT = target - nums[i]
+    if (sumMap.has(currentT)) {
+      return [sumMap.get(currentT)!, i]
     }
+    sumMap.set(nums[i], i)
   }
   return []
 }
